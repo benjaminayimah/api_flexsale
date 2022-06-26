c41e7f0b959a6215d2746d4f15b6a5d84c9a3e2a
Benjamin <benjaminayimah@gmail.com>
2022-06-26 13:40:18 +0000

    commit

--- config/services.php
+++ config/services.php
@@ -29,5 +29,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'google' => [
+        'client_id' => env('617984689362-02931j85j49mm913mn3lf72j4njggajg.apps.googleusercontent.com'),
+        'client_secret' => env('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'),
+        'redirect' => 'http://localhost:8080/',
+    ],
 
 ];
